12bcac5032ea516139020c02cbab3ff31dcec204
Saniya Maheshwari <saniya.mah@gmail.com>
2022-06-11 19:40:20 +0530

    Language code checks
     - An invalid language code i.e. one that is not two-letters long will
       raise a `ValueError` in both `_load_stopwords()` and `_load_case()`.
     - A language code that is valid i.e. is two-letters long but for whom a
       corresponding `stopwords_xy.txt` file is not found, will raise a
       `RuntimeError` regretting lack of support for the language.

--- src/keywey/language.py
+++ src/keywey/language.py
@@ -3,13 +3,21 @@ from typing import Optional
 
 
 def _load_stopwords(lang: Optional[str] = 'en') -> list[str]:
-    contents = files('data.stopwords').joinpath(f'stopwords_{lang}.txt').read_text()
+    try:
+        contents = files('data.stopwords').joinpath(f'stopwords_{lang}.txt').read_text()
+    except FileNotFoundError:
+        if len(lang) != 2:
+            raise ValueError(f"invalid language code '{lang}'")
+        else:
+            raise RuntimeError(f"no support for language with code '{lang}'")
     contents = contents.strip()
     stopwords = [word.strip() for word in contents.split('\n')]
     return stopwords
 
 
 def _load_case(lang: Optional[str] = 'en') -> str:
+    if len(lang) != 2:
+        raise ValueError(f"invalid language code '{lang}'")
     contents = files('data').joinpath('case.txt').read_text()
     contents = contents.strip()
     langs_with_case = [lang.strip() for lang in contents.split('\n')]
--- tests/test_language.py
+++ tests/test_language.py
@@ -3,7 +3,7 @@ import unittest
 from keywey.language import _load_stopwords, _load_case
 
 
-class TestLoadFunctions(unittest.TestCase):
+class TestLoadStopwords(unittest.TestCase):
 
     def test_load_stopwords_en_hi(self):
         for lang, num_stopwords in zip(['en', 'hi'], [179, 264]):
@@ -11,6 +11,24 @@ class TestLoadFunctions(unittest.TestCase):
                 stopwords = _load_stopwords(lang=lang)
                 self.assertEqual(len(stopwords), num_stopwords)
 
+    def test_load_stopwords_invalid_code(self):
+        with self.assertRaisesRegex(ValueError, "invalid language code 'zzz'"):
+            _load_stopwords(lang='zzz')
+        with self.assertRaisesRegex(ValueError, "invalid language code 'z'"):
+            _load_stopwords(lang='z')
+
+    def test_load_stopwords_no_support(self):
+        with self.assertRaisesRegex(RuntimeError, "no support for language with code 'zz'"):
+            _load_stopwords(lang='zz')
+
+class TestLoadCase(unittest.TestCase):
+
     def test_load_case_en_hi(self):
         self.assertEqual(_load_case(lang='en'), 'exists')
         self.assertEqual(_load_case(lang='hi'), 'notdefined')
+
+    def test_load_case_invalid_code(self):
+        with self.assertRaisesRegex(ValueError, "invalid language code 'zzz'"):
+            _load_case(lang='zzz')
+        with self.assertRaisesRegex(ValueError, "invalid language code 'z'"):
+            _load_case(lang='z')
